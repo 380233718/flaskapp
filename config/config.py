efd6f75aa72d945d63690d4d8013d7c6dc4e38c3
stephen <380233718@qq.com>
2017-09-18 22:00:06 +0800

    change config

--- config/config.py
+++ config/config.py
@@ -1,3 +1,19 @@
 #-*- coding:utf-8 -*-
+import os
 
+#set debug
 DEBUG = True
+
+#set session salt
+SECKET_KEY = os.urandom(24)
+
+#set mysql url
+HOSTNAME = '127.0.0.1'
+PORT = '3306'
+USER = 'root'
+PASSWORD = ''
+DATABASE = 'flask_db'
+DB_RUL = 'mysql://{}:{}@{}/{}?charset=utf8'.format(USER,PASSWORD,HOSTNAME,DATABASE)
+SQLALCHEMY_DATABASE_URI = DB_RUL
+SQLALCHEMY_COMMIT_ON_TEARDOWN = True
+
---
+++ manager.py
@@ -0,0 +1,8 @@
+#-*- coding:utf-8 -*-
+
+from flask_script import Manager
+from flask_migrate import Migrate
+
+manager = Manager()
+
+migrate = Migrate()
